92c67aefbf8ced0bce9ceae67d286ed0bd57b161
Ouambo Patrick <ouambotakouam@gmail.com>
2024-02-06 13:25:56 +0100

    Suppression et mise a jour des migrations

--- users/models.py
+++ users/models.py
@@ -7,12 +7,8 @@ class User(AbstractUser):
     phone = models.CharField(max_length=50, blank=True, null=True)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
 
-    class Meta:
-        permissions = []
-
-# Add related_name to groups and user_permissions fields
-User._meta.get_field('groups').remote_field.related_name = 'user_groups'
-User._meta.get_field('user_permissions').remote_field.related_name = 'user_permissions_set'
+    def __str__(self):
+        return self.username
 
 
 class Profile(models.Model):
@@ -26,8 +22,4 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.username
-
-    # @property
-    # def display_role(self):
-    #     return dict(self.CHOICES).get(self.role, 'Undefined')
+        return self.role
